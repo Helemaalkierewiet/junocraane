204d8b656c4f69fecfdfcf2cb4091943504ac91a
Juno <junocraane@gmail.com>
2024-04-04 22:49:35 +0200

    PLS BE FIXED

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+window.onload = init;
 
+function init() {
+    const images = document.querySelectorAll('.container-showcase img');
+    console.log('DOM loaded');
+    images.forEach(img => {
+        img.addEventListener('mouseover', increaseZIndex);
+        img.addEventListener('mouseout', resetZIndex);
+    });
+}
+
+function increaseZIndex() {
+    this.style.zIndex = '1000';
+    console.log('increased z-index to 1000');
+}
+
+function resetZIndex() {
+    this.style.zIndex = '1';
+    console.log('reset z-index to 1');
+}
