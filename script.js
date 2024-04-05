ca989e785e94cdba528b14faa408e804530c0755
Juno <junocraane@gmail.com>
2024-04-05 19:36:00 +0200

    kk veel shit en keyframes teogevoegd

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-
-// gang pasted
-
 window.onload = init;
 
 function init() {
     const images = document.querySelectorAll('.container-showcase img');
     console.log('DOM loaded');
     images.forEach(img => {
-        img.addEventListener('mouseover', increaseZIndex);
-        img.addEventListener('mouseout', resetZIndex);
+        // Store the original width of each image
+        img.dataset.originalWidth = getComputedStyle(img).width;
+        img.addEventListener('mouseover', increaseSizeAndZIndex);
+        img.addEventListener('mouseout', resetSizeAndZIndex);
     });
 }
 
-function increaseZIndex() {
+function increaseSizeAndZIndex() {
     this.style.zIndex = '1000';
-    console.log('increased z-index to 1000');
+    this.style.width = '20vw';
+    console.log('increased z-index to 1000 and scaled up');
 }
 
-function resetZIndex() {
+function resetSizeAndZIndex() {
     this.style.zIndex = '1';
-    console.log('reset z-index to 1');
+    // Reset the width to the original value
+    this.style.width = this.dataset.originalWidth;
+    console.log('reset z-index to 1 and scaled down');
 }
